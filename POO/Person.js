e59913ba6cc0315ff0178e62e52244efcc2dcd19
Manuela-Mariscotti <you@example.com>
2022-07-19 16:58:05 +0200

    retos 1 a 6 terminados

--- POO/Person.js
+++ POO/Person.js
@@ -6,26 +6,27 @@
         this.yearOfBirth = yearOfBirth;
         this.hobbies = new Array;
     }
+    
     getIMC(){
-        return this.weight/(this.height**2)
+        return this.weight/((this.height**2)/10000)
     }
+
     printAll(){
-        //console.log(this) ;)
+        //console.log(this)
         console.log(`nombre - ${this.name}`);
         console.log(`weight - ${this.weight}`);
         console.log(`height - ${this.height}`);
         console.log(`yearOfBirth - ${this.yearOfBirth}`);
         console.log(`hobbies - ${this.hobbies}`);
     }
+
     getAge(){
         let date = new Date().getFullYear();
         return date-this.yearOfBirth;
     }
+    
     printHobbies(){
-        for (let i = 0; i < this.hobbies.length; i++) {
-            console.log(this.hobbies[i])
-            
-        }
+        console.log(this.hobbies) 
     }
 }
-export const module={Person}
+export {Person}
--- POO/personTest.js
+++ POO/personTest.js
@@ -1,4 +1,8 @@
-import Person from "./Person.js";
+import {Person} from "./Person.js";
 
-let persona1 = new Person ("manu",57,160,1987);
+let persona1 = new Person ("Manuela",57,160,1987);
+console.log(persona1.getIMC());
+console.log(persona1.getAge());
+persona1.hobbies.push("dibujar","caminar","programar");
+persona1.printHobbies();
 persona1.printAll();
