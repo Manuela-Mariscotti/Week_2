9fe6897179cc90ad7bbcffaccf1ba7763fe05c88
Manuela-Mariscotti <you@example.com>
2022-07-20 14:47:13 +0200

    prueba

--- Typescript/contactsTest.ts
+++ Typescript/contactsTest.ts
@@ -3,7 +3,8 @@ import {Person} from "./Person"
 
 let persona1 = new Person("Manu",35,"Santa ceclia, 1");
 let persona2 = new Person("Alicia",10,"c/del Aburrimiento");
-let persona3 = new Person("Conejo Blanco",50,"Pais de las maravillas");
+let persona3 = new Person("Conejo Blanco",60,"Pais de las maravillas");
+let persona4 = new Person("Reina de Corazones",50,"El Castillo s/n")
 
 let contacto = new Contacts;
 contacto.people.push(persona1);
